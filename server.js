293e243cfb3f92e615d87f749dc59c8abf40d42f
Vietweb82 <nguyenquocviet2018ca@gmail.com>
2025-08-05 13:05:36 +0700

    update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express on Render!');
+  res.send('Hello from Express on Render!Server is active');
 });
 
 app.listen(port, () => {
